b1eb39855bfe0c02023fa1283c046d30dc3bc937
Rg <daimaldd@gmail.com>
2018-11-06 23:30:48 +0800

    add default for Zero

--- src/types.rs
+++ src/types.rs
@@ -2,5 +2,5 @@ use rmps::decode::Error;
 use rmps::{Deserializer, Serializer};
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Default, Deserialize, Serialize)]
 pub struct Zero;
